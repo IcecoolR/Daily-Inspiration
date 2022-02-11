2538b8c238c30a0eac577a2cc699561c59078386
IcecoolR <45355900+IcecoolR@users.noreply.github.com>
2022-02-11 17:11:14 +0000

    Items pushed

---
+++ scripts.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+
+    const today = new Date();
+
+    const generator = new Math.seedrandom('' + today.getUTCDate() + today.getUTCMonth() + today.getUTCFullYear() );
+    const randomNumber = generator();
+
+    const quoteObject = data[ Math.trunc( randomNumber * (data.length + 1) ) ];
+
+    quote.innerHTML = "\"" + quoteObject.text + "\"";
+
+    if (quoteObject.author == null) {
+
+      author.innerHTML = "- (Unknown author)"
+
+    } else {
+
+      author.innerHTML = "- " + quoteObject.author;
+
+    }
+
+  });
