7cc8e4e7f20c6ed4e2e9b4333379b04dfc829769
Richard <IcecoolRi@gmail.com>
2023-01-03 09:33:08 +0000

    Changed Background Colour
    Changed the background colour to be more consistent to Take That 1st Step. Added temporary quote

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,29 +3,29 @@
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 
-fetch("https://type.fit/api/quotes")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
+// fetch("https://type.fit/api/quotes")
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(data) {
 
-    const today = new Date();
+//     const today = new Date();
 
-    const generator = new Math.seedrandom('' + today.getUTCDate() + today.getUTCMonth() + today.getUTCFullYear() );
-    const randomNumber = generator();
+//     const generator = new Math.seedrandom('' + today.getUTCDate() + today.getUTCMonth() + today.getUTCFullYear() );
+//     const randomNumber = generator();
 
-    const quoteObject = data[ Math.trunc( randomNumber * (data.length + 1) ) ];
+//     const quoteObject = data[ Math.trunc( randomNumber * (data.length + 1) ) ];
 
-    quote.innerHTML = "\"" + quoteObject.text + "\"";
+//     quote.innerHTML = "\"" + quoteObject.text + "\"";
 
-    if (quoteObject.author == null) {
+//     if (quoteObject.author == null) {
 
-      author.innerHTML = "- (Unknown author)"
+//       author.innerHTML = "- (Unknown author)"
 
-    } else {
+//     } else {
 
-      author.innerHTML = "- " + quoteObject.author;
+//       author.innerHTML = "- " + quoteObject.author;
 
-    }
+//     }
 
-  });
+//   });
